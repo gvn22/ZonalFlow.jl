44212cc7711dd3846f8e21616394433a0c3fa008
Girish Nivarti <nvgirishs@gmail.com>
2020-11-09 14:06:06 +0000

    Correcting parameter output during test

--- test/qlnl_tests.jl
+++ test/qlnl_tests.jl
@@ -5,6 +5,8 @@ using Test
     @info "Testing QL/CE2 conformity..."
     lx = 4.0*Float64(pi);
     ly = 2.0*Float64(pi);
+    Ω = 2.0*Float64(pi)
+
     Λ = 0
     for nx=4:4
         for ny=nx:4
@@ -14,16 +16,12 @@ using Test
 
                         @info "Parameters: θ = $θ, τ = $τ, Ξ = $Ξ"
 
-                        Ω = 2.0*Float64(pi)
-                        θ = θ*Float64(pi)
-                        β = 2.0*Ω*cos(θ)
-                        τ = τ/Ω
-                        Ξ = Ξ*Ω
-                        ζ0 = ic_pert_eqm(lx,ly,nx,ny,Ξ); # one ic for all
+                        β = 2.0*Ω*cos(θ*Float64(pi))
+                        ζ0 = ic_pert_eqm(lx,ly,nx,ny,Ξ*Ω); # one ic for all
 
-                        sol2 = gql(lx,ly,nx,ny,Λ,Ξ,β,τ,t_end=500.0,dt=0.001,ic=ζ0);
-                        sol3 = gce2(lx,ly,nx,ny,Λ,Ξ,β,τ,t_end=500.0,ic=ζ0,dt=0.001);
-                        sol4 = gce2(lx,ly,nx,ny,Λ,Ξ,β,τ,t_end=500.0,ic=ζ0,dt=0.001,poscheck=true);
+                        sol2 = gql(lx,ly,nx,ny,Λ,Ξ*Ω,β,τ/Ω,t_end=500.0,dt=0.001,ic=ζ0);
+                        sol3 = gce2(lx,ly,nx,ny,Λ,Ξ*Ω,β,τ/Ω,t_end=500.0,ic=ζ0,dt=0.001);
+                        sol4 = gce2(lx,ly,nx,ny,Λ,Ξ*Ω,β,τ/Ω,t_end=500.0,ic=ζ0,dt=0.001,poscheck=true);
                         @test sol2.u[end][:,1:Λ+1] ≈ sol3.u[end].x[1] atol = 1e-6
                         @test sol2.u[end][:,1:Λ+1] ≈ sol4.u[end].x[1] atol = 1e-6
                         P2,O2 = zonalenergy(lx,ly,nx,ny,sol2.u);
@@ -43,6 +41,8 @@ end
     @info "Testing NL/GQL(M)/GCE2(M) conformity..."
     lx = 4.0*Float64(pi);
     ly = 2.0*Float64(pi);
+    Ω = 2.0*Float64(pi)
+
     for nx=4:4
         for ny=nx:4
             for θ in [0.0,1.0/6.0,1.0/3.0]
@@ -51,20 +51,16 @@ end
 
                         @info "Parameters: θ = $θ, τ = $τ, Ξ = $Ξ"
 
-                        Ω = 2.0*Float64(pi)
-                        θ = θ*Float64(pi)
-                        β = 2.0*Ω*cos(θ)
-                        τ = τ/Ω
-                        Ξ = Ξ*Ω
-                        ζ0 = ic_pert_eqm(lx,ly,nx,ny,Ξ); # one ic for all
+                        β = 2.0*Ω*cos(θ*Float64(pi))
+                        ζ0 = ic_pert_eqm(lx,ly,nx,ny,Ξ*Ω); # one ic for all
 
-                        sol1 = nl(lx,ly,nx,ny,Ξ,β,τ,t_end=500.0,dt=0.001,ic=ζ0);
+                        sol1 = nl(lx,ly,nx,ny,Ξ*Ω,β,τ/Ω,t_end=500.0,dt=0.001,ic=ζ0);
                         P1,O1 = zonalenergy(lx,ly,nx,ny,sol1.u);
 
                         Λ = nx-1
-                        sol2 = gql(lx,ly,nx,ny,Λ,Ξ,β,τ,t_end=500.0,dt=0.001,ic=ζ0);
-                        sol3 = gce2(lx,ly,nx,ny,Λ,Ξ,β,τ,t_end=500.0,ic=ζ0,dt=0.001);
-                        sol4 = gce2(lx,ly,nx,ny,Λ,Ξ,β,τ,t_end=500.0,ic=ζ0,dt=0.001,poscheck=true);
+                        sol2 = gql(lx,ly,nx,ny,Λ,Ξ*Ω,β,τ/Ω,t_end=500.0,dt=0.001,ic=ζ0);
+                        sol3 = gce2(lx,ly,nx,ny,Λ,Ξ*Ω,β,τ/Ω,t_end=500.0,ic=ζ0,dt=0.001);
+                        sol4 = gce2(lx,ly,nx,ny,Λ,Ξ*Ω,β,τ/Ω,t_end=500.0,ic=ζ0,dt=0.001,poscheck=true);
                         @test sol1.u[end] ≈ sol2.u[end] atol = 1e-6
                         @test sol1.u[end] ≈ sol3.u[end].x[1] atol = 1e-6
                         @test sol1.u[end] ≈ sol4.u[end].x[1] atol = 1e-6
