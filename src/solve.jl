3a56a0c132169c53b78f283e75a6919ad9a07bfa
Girish Nivarti <nvgirishs@gmail.com>
2021-04-09 16:48:47 +0100

    Consistency in u,p,t order with diffeq
    get_de_probalg -> call u0,tspan,p as in ODEDiffEq

--- src/solve.jl
+++ src/solve.jl
@@ -16,18 +16,18 @@ get_de_eqs(::GQL) = gql_eqs!,unit_eqs!
 get_de_eqs(::GCE2) = gce2_eqs!,unit_gce2_eqs!
 get_de_eqs(::CE2) = ce2_eqs!
 
-function get_de_probalg(prob,eqs,u0,p,t)
+function get_de_probalg(prob,eqs,u0,t,p)
     f,g = get_de_eqs(eqs)
     ODEProblem(f,u0,t,p), RK4()
 end
 
-function get_de_probalg(prob::BetaPlane{T,Stochastic{T}},eqs,u0,p,t) where T
+function get_de_probalg(prob::BetaPlane{T,Stochastic{T}},eqs,u0,t,p) where T
     f = get_de_eqs(eqs)
     W0 = zeros(eqs,size(prob.d))
     SDEProblem(f...,u0,t,p,noise=noise!(t[0],W0)), EulerHeun()
 end
 
-function get_de_probalg(prob::BetaPlane{T,Stochastic{T}},eqs::CE2,u0,p,t) where T
+function get_de_probalg(prob::BetaPlane{T,Stochastic{T}},eqs::CE2,u0,t,p) where T
     f = get_de_eqs(eqs)
     ODEProblem(f,u0,t,p), Heun()
 end
@@ -35,6 +35,6 @@ end
 function solve(prob,eqs;tspan,kwargs...)
     u0 = get_de_ic(prob,eqs)
     p = get_de_params(prob,eqs)
-    _prob,_alg = get_de_probalg(prob,eqs,u0,p,tspan)
+    _prob,_alg = get_de_probalg(prob,eqs,u0,tspan,p)
     solve(_prob,_alg,kwargs...)
 end
