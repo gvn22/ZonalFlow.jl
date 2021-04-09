73b3828791445778c51293fe513b88824215a67e
Girish Nivarti <nvgirishs@gmail.com>
2021-04-09 11:05:28 +0100

    Remove old comments and comment IC code
    ICs required will be overloads of zeros and rand

--- src/ranges.jl
+++ src/ranges.jl
@@ -1,5 +1,5 @@
 """
-    Ranges:
+    Multi-loop iterators
 """
 function tworange(nx,ny)
     m=1:nx
@@ -9,9 +9,9 @@ end
 fourrange(nx,ny) = Iterators.product(tworange(nx,ny),tworange(nx,ny))
 
 arange(ny) = ny+1:2ny-1
-brange2(nx,ny) = tworange(nx,ny)
+brange(nx,ny) = tworange(nx,ny)
 
-function cprange2(nx,ny)
+function cprange(nx,ny)
     m1lim(((n2,m2),(n1,m1))) = m1-1>=1
     m2lim(((n2,m2),(n1,m1))) = 0<=m2-1<=min(m1-1,nx-m1)
     n2lim(((n2,m2),(n1,m1))) = 1-n1<=n2-ny<=min(ny-1,1-n1)
@@ -19,7 +19,7 @@ function cprange2(nx,ny)
     fourrange(nx,ny) |> filter(m1lim) |> filter(m2lim) |> filter(n2lim)
 end
 
-function cmrange2(nx,ny)
+function cmrange(nx,ny)
     m1lim(((n2,m2),(n1,m1))) = 2<=m1<=nx
     m2lim(((n2,m2),(n1,m1))) = 1<=m2<=m1
     function n2lim(((n2,m2),(n1,m1)))
@@ -30,43 +30,43 @@ function cmrange2(nx,ny)
     fourrange(nx,ny) |> filter(m1lim) |> filter(m2lim) |> filter(n2lim)
 end
 
-function brange(nx,ny)
-    b = Vector{Tuple{Int,Int}}(undef, nx*(2ny-1))
-    for m = 0:nx-1
-        nmin = m == 0 ? 1 : -ny+1
-        for n=nmin:ny-1
-            b[m*(2ny-1) + n] = (n+ny,m+1)
-        end
-    end
-    b
-end
-
-function cprange(nx,ny)
-    c⁺ = Tuple{Int,Int,Int,Int,Int,Int}[]
-    for m1=1:nx-1
-        for n1=-ny+1:ny-1
-            for m2=0:min(m1,nx-1-m1)
-                n2min = m2 == 0 ? 1 : -ny+1
-                for n2=max(n2min,-ny+1-n1):min(ny-1,-ny+1-n1)
-                    push!(c⁺,(n1+n2+ny,m1+m2+1,n2+ny,m2+1,n1+ny,m1+1))
-                end
-            end
-        end
-    end
-end
-
-function cmrange(nx,ny)
-    c⁻ = Tuple{Int,Int,Int,Int,Int,Int}[]
-    for m1=1:nx-1
-        for n1=-ny+1:ny-1
-            for m2=0:m1
-                n2min = m2 == 0 ? 1 : -ny+1
-                n2max = m2 == m1 ? n1 - 1 : ny-1
-                for n2=max(n2min,n1-ny+1):min(n2max,n1+ny-1)
-                    push!(c⁻,(n1-n2+ny,m1-m2+1,n2+ny,m2+1,n1+ny,m1+1))
-                end
-            end
-        end
-    end
-    c⁻
-end
+# function brange(nx,ny)
+#     b = Vector{Tuple{Int,Int}}(undef, nx*(2ny-1))
+#     for m = 0:nx-1
+#         nmin = m == 0 ? 1 : -ny+1
+#         for n=nmin:ny-1
+#             b[m*(2ny-1) + n] = (n+ny,m+1)
+#         end
+#     end
+#     b
+# end
+#
+# function cprange(nx,ny)
+#     c⁺ = Tuple{Int,Int,Int,Int,Int,Int}[]
+#     for m1=1:nx-1
+#         for n1=-ny+1:ny-1
+#             for m2=0:min(m1,nx-1-m1)
+#                 n2min = m2 == 0 ? 1 : -ny+1
+#                 for n2=max(n2min,-ny+1-n1):min(ny-1,-ny+1-n1)
+#                     push!(c⁺,(n1+n2+ny,m1+m2+1,n2+ny,m2+1,n1+ny,m1+1))
+#                 end
+#             end
+#         end
+#     end
+# end
+#
+# function cmrange(nx,ny)
+#     c⁻ = Tuple{Int,Int,Int,Int,Int,Int}[]
+#     for m1=1:nx-1
+#         for n1=-ny+1:ny-1
+#             for m2=0:m1
+#                 n2min = m2 == 0 ? 1 : -ny+1
+#                 n2max = m2 == m1 ? n1 - 1 : ny-1
+#                 for n2=max(n2min,n1-ny+1):min(n2max,n1+ny-1)
+#                     push!(c⁻,(n1-n2+ny,m1-m2+1,n2+ny,m2+1,n1+ny,m1+1))
+#                 end
+#             end
+#         end
+#     end
+#     c⁻
+# end
