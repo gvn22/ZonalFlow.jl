91e170726991de29e60582a588628c86cf3b275a
Girish Nivarti <nvgirishs@gmail.com>
2021-04-20 13:40:36 +0100

    Tests for conservation laws and coeff matching
    These are included in runtests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,4 @@
 using SafeTestsets
 
-@safetestset "Analytical 2x2 example test" begin include("twobytwo_tests.jl") end
-@safetestset "QL/CE2 and NL/GQL(M)/GCE2(M) tests" begin include("qlnl_tests.jl") end
-@safetestset "GQL(Λ)/GCE2(Λ) tests" begin include("gql_tests.jl") end
+@safetestset "Conservation of energy and enstrophy" begin include("conservation.jl") end
+@safetestset "Matching coefficients with old method" begin include("coeffs.jl") end
