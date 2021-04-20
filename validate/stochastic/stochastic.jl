80b3c5e314d0a67cf2c26adc3937d069f4254e26
Girish Nivarti <nvgirishs@gmail.com>
2021-04-20 13:41:06 +0100

    Validation script for stochastic jets

--- validate/stochastic/stochastic.jl
+++ validate/stochastic/stochastic.jl
@@ -6,27 +6,21 @@ coeffs  = Coefficients(Ω=2.0π,θ=0.0,μ=0.01,ν=0.0,ν₄=1.0,linear=true);
 forcing = Stochastic(kf=3,dk=1,ε=0.01);
 prob    = BetaPlane(domain,coeffs,forcing);
 
-tspan   = (0.0,500.0);
+tspan   = (0.0,10000.0);
 tsargs  = (
             dt=0.001,
             adaptive=false,
             progress=true,
             progress_steps=100000,
             save_everystep=false,
-            saveat=100
+            saveat=20
            );
 
-eqs     = [NL(),GQL(Λ=0),CE2()];
-labels  = ["nl_linear","ql_linear","ce2_linear"];
+eqs     = [GQL(Λ=0),CE2()];
+labels  = ["ql_linear","ce2_linear"];
+sols    = integrate(prob,eqs,tspan;tsargs...);
+write(prob,eqs,sols,dn="validate/stochastic/6x6/",labels=labels)
 
-@time sol1  = integrate(prob,eqs[1],tspan;tsargs...);
-@time sol2  = integrate(prob,eqs[2],tspan;tsargs...);
-@time sol3  = integrate(prob,eqs[3],tspan;tsargs...);
-# @time sol4  = integrate(prob,GQL(Λ=1),tspan;tsargs...);
-# @time sol5  = integrate(prob,GCE2(Λ=1),tspan;tsargs...);
-
-sols = [sol1,sol2,sol3];
-
-write(prob,eqs[1],sols[1],dn="validate/stochastic/",fn=labels[1])
-write(prob,eqs[2],sols[2],dn="validate/stochastic/",fn=labels[2])
-write(prob,eqs[3],sols[3],dn="validate/stochastic/",fn=labels[3])
+eqs     = [NL(),GCE2(Λ=prob.d.nx-1)];
+labels  = ["nl_linear","gce2_linear"];
+write(prob,eqs,sols,dn="validate/stochastic/6x6/",labels=labels)
