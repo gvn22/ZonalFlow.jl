a165a6a3b2f3cec695615b9b5e2270f07d6c6f06
Girish Nivarti <nvgirishs@gmail.com>
2021-04-21 15:01:33 +0100

    Minor change to stochastic test run

--- validate/stochastic/stochastic.jl
+++ validate/stochastic/stochastic.jl
@@ -6,7 +6,7 @@ coeffs  = Coefficients(Ω=2.0π,θ=0.0,μ=0.01,ν=0.0,ν₄=1.0,linear=true);
 forcing = Stochastic(kf=3,dk=1,ε=0.01);
 prob    = BetaPlane(domain,coeffs,forcing);
 
-tspan   = (0.0,10000.0);
+tspan   = (0.0,1000.0);
 tsargs  = (
             dt=0.001,
             adaptive=false,
