09bf3cec31cc69e6fa035639f90b5e5bc63772ec
Girish Nivarti <nvgirishs@gmail.com>
2021-04-27 15:13:28 +0100

    Removed unnecessary tests
    New tests check conservation

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,5 @@
 using SafeTestsets
 
 @safetestset "Conservation of energy and enstrophy" begin include("conservation.jl") end
-@safetestset "Matching coefficients with old method" begin include("coeffs.jl") end
+# @safetestset "Matching coefficients with old method" begin include("coeffs.jl") end
+# @safetestset "Matching parameters with new structures" begin include("structures.jl") end
