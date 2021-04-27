dfae0e21f0a7c7f73dd093f05479d2e8a6175bb3
Girish Nivarti <nvgirishs@gmail.com>
2021-04-27 12:17:14 +0100

    Adding ipynb notebooks and stochastic tests
    Stochastic tests look good except for a small
    difference in QL and CE2, possibly due to
    timestepping algorithm, and relaxation time

--- validate/stochastic/stochastic.jl
+++ validate/stochastic/stochastic.jl
@@ -1,67 +1,71 @@
 using Revise
 using ZonalFlow
+using JLD2
 
-domain  = Domain(extent=(2π,2π),res=(6,6));
-tspan   = (0.0,1000.0);
+tspan   = (0.0,2000.0);
 tsargs  = (
             dt=0.001,
             adaptive=false,
             progress=true,
             progress_steps=100000,
             save_everystep=false,
-            saveat=50
+            saveat=50,
+            save_noise=false
            );
 
-# Linear
-domain  = Domain(extent=(2π,2π),res=(6,6));
-coeffs  = Coefficients(Ω=5.0,θ=0.0,μ=0.01,ν=0.0,ν₄=1.0,linear=true);
-forcing = Stochastic(kf=3,dk=1,ε=0.01);
+# 6x6
+# Couldn't find jets <- res possibly too small
+
+# 8x8
+domain  = Domain(extent=(2π,2π),res=(8,8));
+coeffs  = Coefficients(Ω=5.0,θ=0.0,μ=0.01,ν=0.0,ν₄=1.0,linear=false);
+forcing = Stochastic(kf=5,dk=2,ε=0.005);
 prob    = BetaPlane(domain,coeffs,forcing);
 
-eqs     = [GQL(Λ=0),CE2()];
-labels  = ["ql_linear","ce2_linear"];
-sols    = integrate(prob,eqs,tspan;tsargs...);
+dn      = "validate/stochastic/8x8/"
 
-write(prob,eqs,sols,dn="validate/stochastic/6x6/",labels=labels)
+eqs     = [NL(),GQL(Λ=0),CE2()];
+labels  = ["nl","ql","ce2"];
 
-eqs     = [NL(),GQL(Λ=prob.d.nx-1),GCE2(Λ=prob.d.nx-1)];
-labels  = ["nl_linear","gql_5_linear","gce2_5_linear"];
-sols    = integrate(prob,eqs,tspan;tsargs...);
-write(prob,eqs,sols,dn="validate/stochastic/6x6/",labels=labels)
+nl      = integrate(prob,eqs[1],tspan;tsargs...)
+write(prob,eqs[1],nl,dn=dn,fn=labels[1])
 
-# Nonlinear
-coeffs  = Coefficients(Ω=2π,θ=0.0,μ=0.01,ν=0.0,ν₄=1.0,linear=false);
-forcing = Stochastic(kf=3,dk=1,ε=0.001);
+ql      = integrate(prob,eqs[2],tspan;tsargs...)
+write(prob,eqs[2],ql,dn=dn,fn=labels[2])
+
+ce2      = integrate(prob,eqs[3],tspan;u0=ql.u[end],tsargs...)
+write(prob,eqs[3],ce2,dn=dn,fn=labels[3])
+
+# sols    = integrate(prob,eqs,tspan;tsargs...)
+# write(prob,eqs,sols,dn=dn,labels=labels)
+
+eqs     = [GQL(Λ=1),GCE2(Λ=1)];
+labels  = ["gql_1","gce2_1"];
+
+gql      = integrate(prob,eqs[1],tspan;tsargs...)
+write(prob,eqs[1],gql,dn=dn,fn=labels[1])
+
+gce2      = integrate(prob,eqs[2],tspan;tsargs...)
+write(prob,eqs[1],gql,dn=dn,fn=labels[1])
+
+# sols    = integrate(prob,eqs,tspan;tsargs...)
+# write(prob,eqs,sols,dn=dn,labels=labels)
+
+# 12x12
+domain  = Domain(extent=(2π,2π),res=(12,12));
+coeffs  = Coefficients(Ω=5.0,θ=0.0,μ=0.01,ν=0.0,ν₄=1.0,linear=false);
+forcing = Stochastic(kf=8,dk=2,ε=0.001);
 prob    = BetaPlane(domain,coeffs,forcing);
 
-ql      = integrate(prob,GQL(0),tspan;tsargs...)
-ce2     = integrate(prob,CE2(),tspan;u0=ql.u[1],tsargs...)
-eqs     = [GQL(Λ=0),CE2()];
-labels  = ["ql","ce2"];
-# sols    = integrate(prob,eqs,tspan;tsargs...);
-sols    = [ql,ce2];
-write(prob,eqs,sols,dn="validate/stochastic/6x6/",labels=labels)
-
-nl      = integrate(prob,NL(),tspan;tsargs...)
-gce2    = integrate(prob,GCE2(Λ=prob.d.nx-1),tspan;u0=nl.u[end],tsargs...)
-
-eqs     = [NL(),GQL(Λ=prob.d.nx-1),GCE2(Λ=prob.d.nx-1)];
-labels  = ["nl","gql_5","gce2_5"];
-sols    = integrate(prob,eqs,tspan;tsargs...);
-write(prob,eqs,sols,dn="validate/stochastic/6x6/",labels=labels)
-
-# Linear
-domain  = Domain(extent=(2π,2π),res=(6,6));
-coeffs  = Coefficients(Ω=2π,θ=0.0,μ=0.01,ν=0.0,ν₄=1.0,linear=true);
-forcing = Stochastic(kf=3,dk=1,ε=0.001);
-prob    = BetaPlane(domain,coeffs,forcing);
+dn      = "validate/stochastic/12x12/"
 
 eqs     = [GQL(Λ=0),CE2()];
-labels  = ["ql_linear","ce2_linear"];
-sols    = integrate(prob,eqs,tspan;tsargs...);
-write(prob,eqs,sols,dn="validate/stochastic/6x6/",labels=labels)
+labels  = ["ql_long","ce2_long"];
 
-eqs     = [NL(),GQL(Λ=prob.d.nx-1),GCE2(Λ=prob.d.nx-1)];
-labels  = ["nl_linear","gql_5_linear","gce2_5_linear"];
-sols    = integrate(prob,eqs,tspan;tsargs...);
-write(prob,eqs,sols,dn="validate/stochastic/6x6/",labels=labels)
+ql      = integrate(prob,eqs[1],tspan;tsargs...)
+write(prob,eqs[1],ql,dn=dn,fn=labels[1])
+@save dn*labels[1]*".jld2" ql
+
+ce2     = integrate(prob,CE2(),tspan;tsargs...)
+write(prob,eqs[2],ce2,dn=dn,fn=labels[2])
+@save dn*labels[2]*".jld2" ce2
