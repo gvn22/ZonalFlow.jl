1da34dd0c20942d56bba1b6dd05e6aef27bd8c58
Girish Nivarti <nvgirishs@gmail.com>
2021-04-30 11:19:50 +0100

    Adding point jet scripts and noise script from earlier on
    Point jet on 12x12 grid with tau = 20

--- validate/pointjet/pointjet.jl
+++ validate/pointjet/pointjet.jl
@@ -11,11 +11,15 @@ tsargs  = (
             saveat=50
            );
 
-domain  = Domain(extent=(2π,2π),res=(6,6));
+domain  = Domain(extent=(2π,2π),res=(12,12));
 coeffs  = Coefficients(Ω=2π,θ=0.0,μ=0.05,ν=0.0,ν₄=0.0);
 forcing = PointJet(Ξ=1.0,Δθ=0.1,τ=20.0);
 prob    = BetaPlane(domain,coeffs,forcing);
 
-# eqs     = [NL(),GQL(Λ=prob.d.nx-1),GCE2(Λ=prob.d.nx-1),QL(),CE2(),GCE2(Λ=0)];
-# labels  = ["nl","gql","gce2","ql","ce2","gce2_0"];
-# sols    = integrate(prob,eqs,tspan;tsargs...);
+eqs     = [NL(),GQL(Λ=prob.d.nx-1),GCE2(Λ=prob.d.nx-1),GQL(0),CE2()];
+labels  = ["nl","gql","gce2","ql","ce2"];
+sols    = integrate(prob,eqs,tspan;tsargs...);
+
+dn      = "validate/pointjet/12x12/"
+
+write(prob,eqs,ql,dn=dn,labels=labels)
