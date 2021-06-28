6b4dc09364a8d88cf9c4585b4cea6f26a838e4d9
Girish Nivarti <nvgirishs@gmail.com>
2021-06-28 15:45:27 +0100

    CE2 rank computations made in notebook
    Second cumulant plots

--- src/writers.jl
+++ src/writers.jl
@@ -9,25 +9,32 @@ Base.write(prob,eqs::Vector{AbstractEquations},sols;dn::String,labels::Vector{St
 
 tonpz(u) = reshape(cat(u...,dims=length(size(u[1]))),size(u[1])...,length(u))
 
-function dumpscalars(prob,sol;t0=200.0)
+function dumpscalars(prob,sol;t0=500.0)
     Et = energy.(Ref(prob.d),sol.u)
     Zt = enstrophy.(Ref(prob.d),sol.u)
     Emt = zonalenergy.(Ref(prob.d),sol.u) |> tonpz
     Em0nt = modalenergy.(Ref(prob.d),sol.u,m=0) |> tonpz
     Em1nt = modalenergy.(Ref(prob.d),sol.u,m=1) |> tonpz
     Em2nt = modalenergy.(Ref(prob.d),sol.u,m=2) |> tonpz
+    Em0ntav = modalenergy.(Ref(prob.d),sol.u,m=0) |> x->timeaverage(sol.t,x) |> tonpz
+    Em1ntav = modalenergy.(Ref(prob.d),sol.u,m=1) |> x->timeaverage(sol.t,x) |> tonpz
+    Em2ntav = modalenergy.(Ref(prob.d),sol.u,m=2) |> x->timeaverage(sol.t,x) |> tonpz
     Emtav = zonalenergy.(Ref(prob.d),sol.u) |> x->timeaverage(sol.t,x) |> tonpz
-    Dict("t"=>sol.t,"Et"=>Et,"Emt"=>Emt,"Zt"=>Zt,"Emtav"=>Emtav,"Em0nt"=>Em0nt,"Em1nt"=>Em1nt,"Em2nt"=>Em2nt)
+    Dict("t"=>sol.t,"Et"=>Et,"Emt"=>Emt,"Zt"=>Zt,"Emtav"=>Emtav,"Em0nt"=>Em0nt,"Em1nt"=>Em1nt,"Em2nt"=>Em2nt,
+        "Em0ntav"=>Em0ntav,"Em1ntav"=>Em1ntav,"Em2ntav"=>Em2ntav)
 end
 
 function dumpfields(prob,sol)
     Emn = energyspectrum.(Ref(prob.d),sol.u) |> x->timeaverage(sol.t,x) |> tonpz
     Fyym1 = secondcumulant.(Ref(prob.d),sol.u,m=1) |> tonpz
     Fyym2 = secondcumulant.(Ref(prob.d),sol.u,m=2) |> tonpz
-    Vxy = vorticity.(Ref(prob.d),sol.u) |> x->timeaverage(sol.t,x) |> tonpz
+    Fyym3 = secondcumulant.(Ref(prob.d),sol.u,m=3) |> tonpz
+    Vxyav = vorticity.(Ref(prob.d),sol.u) |> x->timeaverage(sol.t,x) |> tonpz
+    Vxy = vorticity.(Ref(prob.d),sol.u) |> tonpz
     Uxy = xvelocity.(Ref(prob.d),sol.u) |> tonpz
-    Vyt = zonalvorticity.(Ref(prob.d),sol.u) |> x->timeaverage(sol.t,x) |> tonpz
-    Dict("t"=>sol.t,"Emn"=>Emn,"Vxy"=>Vxy,"Uxy"=>Uxy,"Vyt"=>Vyt,"Fyym1"=>Fyym1,"Fyym2"=>Fyym2)
+    Vyt = zonalvorticity.(Ref(prob.d),sol.u) |> tonpz
+    Vytav = zonalvorticity.(Ref(prob.d),sol.u) |> x->timeaverage(sol.t,x) |> tonpz
+    Dict("t"=>sol.t,"Emn"=>Emn,"Vxy"=>Vxy,"Vxyav"=>Vxyav,"Uxy"=>Uxy,"Vyt"=>Vyt,"Vytav"=>Vytav,"Fyym1"=>Fyym1,"Fyym2"=>Fyym2,"Fyym3"=>Fyym3)
 end
 
 dumpstats(prob,eqs,sol) = Dict("empty"=>0)
