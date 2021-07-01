533d333fd01ce618127675c5fe05ea2545f512ac
Girish Nivarti <nvgirishs@gmail.com>
2021-07-01 11:30:09 +0100

    Set IC for CE2 to QL not sqrt(QL)
    No requirement for sqrt() as CE2
    is indeed second moment

--- src/ic.jl
+++ src/ic.jl
@@ -7,4 +7,4 @@
 """
 Base.zeros(eqs::AbstractEquations,d) = fill!(similar(eqs,d),0)
 Random.rand(eqs::Union{NL,GQL},d::Domain{T},aη::T=1e-6) where T =  aη*exp.(im*rand!(Uniform(0,2π),similar(eqs,d)))
-Random.rand(eqs::Union{CE2,GCE2},d::Domain{T},aη::T=1e-6) where T = convert(eqs,rand(NL(),d,sqrt(aη)),d)
+Random.rand(eqs::Union{CE2,GCE2},d::Domain{T},aη::T=1e-6) where T = convert(eqs,rand(NL(),d,aη),d)
