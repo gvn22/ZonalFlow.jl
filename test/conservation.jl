57ca0758b2ab97df06989fffa40cdb7211e5c5ec
Girish Nivarti <nvgirishs@gmail.com>
2022-05-25 09:49:02 -0400

    Simplify test for conservation
    Use a fully non-linear test

--- test/conservation.jl
+++ test/conservation.jl
@@ -1,42 +1,36 @@
 using ZonalFlow
 using Test
 
-domain  = Domain(extent=(2π,2π),res=(5,5));
-coeffs  = Coefficients(Ω=2π,θ=0.0,μ=0.0,ν=0.0,ν₄=1.0,linear=true);
-forcing = Stochastic(kf=3,dk=1,ε=0.0);
-prob    = BetaPlane(domain,coeffs,forcing);
-
-tspan   = (0.0,1000.0);
+tspan   = (0.0,200.0);
 tsargs  = (
             dt=0.001,
             adaptive=false,
-            progress=true,
-            progress_steps=100000,
+            progress=false,
+            progress_steps=10000,
             save_everystep=false,
-            saveat=500
+            saveat=50
            );
 
-eqs     = [NL(),CE2()];
-eqs     = append!(eqs,[GQL(Λ=l) for l=0:prob.d.nx-1])
-eqs     = append!(eqs,[GCE2(Λ=l) for l=0:prob.d.nx-1])
-sols    = integrate(prob,eqs,tspan;tsargs...);
+domain  = Domain(extent=(2π,2π),res=(6,6));
+coeffs  = Coefficients(Ω=2π,θ=0.0,μ=0.0,ν=0.0,ν₄=0.0);
 
-@testset "Linear Equations" begin
-        for sol in sols
-                E,Z = energy(length(prob.d)...,size(prob.d)...,sol.t,sol.u);
-                @test E[1] == E[end]
-                @test Z[1] == Z[end]
-        end
-end
+f1 = PointJet(Ξ=0.0,Δθ=0.1,τ=10.0);
+f2 = Kolmogorov(A₁=0.0,A₄=0.0);
+f3 = Stochastic(kf=3,dk=1,ε=0.0);
 
-coeffs  = Coefficients(Ω=2π,θ=0.0,μ=0.0,ν=0.0,ν₄=1.0,linear=false);
-prob    = BetaPlane(domain,coeffs,forcing);
-sols    = integrate(prob,eqs,tspan;tsargs...);
+eqs = [NL(),CE2(),GQL(0)];
+# eqs     = append!(eqs,[GQL(Λ=l) for l=0:prob.d.nx-1])
+# eqs     = append!(eqs,[GCE2(Λ=l) for l=0:prob.d.nx-1])
 
-@testset "Nonlinear Equations" begin
-        for sol in sols
-                E,Z = energy(length(prob.d)...,size(prob.d)...,sol.t,sol.u);
-                @test E[1] == E[end]
-                @test Z[1] == Z[end]
+for f in [f1,f2,f3]
+        prob = BetaPlane(domain,coeffs,forcing);
+        @testset "Forcing type: F1" begin
+                for eq in eqs
+                        sol = integrate(prob,eq,tspan;tsargs...);
+                        E = energy(prob.d,sol.u)
+                        Z = enstrophy(prob.d,sol.u)
+                        @test E[1] == E[end]
+                        @test Z[1] == Z[end]
+                end
         end
 end
