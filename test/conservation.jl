7682d87fee1618dba8c6d7cc776f8e9248b2eb91
Girish Nivarti <nvgirishs@gmail.com>
2022-05-25 10:29:53 -0400

    Further issues in conservation test fixed
    Energy on array of solution snapshots must .

--- test/conservation.jl
+++ test/conservation.jl
@@ -17,20 +17,19 @@ coeffs  = Coefficients(Ω=2π,θ=0.0,μ=0.0,ν=0.0,ν₄=0.0);
 f1 = PointJet(Ξ=0.0,Δθ=0.1,τ=10.0);
 f2 = Kolmogorov(A₁=0.0,A₄=0.0);
 f3 = Stochastic(kf=3,dk=1,ε=0.0);
+fls = ["Point Jet", "Kolmogorov", "Stochastic"]
 
 eqs = [NL(),CE2(),GQL(0)];
 # eqs     = append!(eqs,[GQL(Λ=l) for l=0:prob.d.nx-1])
 # eqs     = append!(eqs,[GCE2(Λ=l) for l=0:prob.d.nx-1])
 
-for forcing in [f1,f2,f3]
+for (label,forcing) in zip(fls,[f1,f2,f3])
         prob = BetaPlane(domain,coeffs,forcing);
-        @testset "Forcing type: F1" begin
-                for eq in eqs
-                        sol = integrate(prob,eq,tspan;tsargs...);
-                        E = energy(prob.d,sol.u)
-                        Z = enstrophy(prob.d,sol.u)
-                        @test E[1] == E[end]
-                        @test Z[1] == Z[end]
-                end
+        for eq in eqs
+                sol = integrate(prob,eq,tspan;tsargs...);
+                E = energy.(prob.d,sol.u)
+                Z = enstrophy.(prob.d,sol.u)
+                @test E[1] == E[end]
+                @test Z[1] == Z[end]
         end
 end
