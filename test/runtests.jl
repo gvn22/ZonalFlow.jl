944137aee2533d969ac6378b58dfbbc5c28784b8
Girish Nivarti <nvgirishs@gmail.com>
2022-05-26 04:01:41 -0400

    Updating tests and including structure tests
    Julia version may help address test fail

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,9 @@
 using SafeTestsets
 
+@safetestset "Running structure tests."
+begin include("structures.jl") end
 @safetestset "Running energy and enstrophy conservation tests." begin
     include("pointjet.jl")
     include("kolmogorov.jl")
-    include("stochastic.jl") 
+    include("stochastic.jl")
 end
