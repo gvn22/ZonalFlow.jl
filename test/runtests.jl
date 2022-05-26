f70218f8f60e85213698b63dec40d1a7d40e177f
Girish Nivarti <nvgirishs@gmail.com>
2022-05-26 04:10:12 -0400

    Fix safetestset call
    Requires indentation?

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,9 @@
 using SafeTestsets
 
 @safetestset "Running structure tests."
-begin include("structures.jl") end
+begin
+    include("structures.jl")
+end
 @safetestset "Running energy and enstrophy conservation tests." begin
     include("pointjet.jl")
     include("kolmogorov.jl")
